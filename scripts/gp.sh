75853cd78a0ab9ddbc754e81c738742816542add
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-11-02 15:23:23 +0100

    fix ref to ARE

--- scripts/gp.sh
+++ scripts/gp.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-if test -f "./h5m_files/ARE.h5m"; then
-  python ./scripts/ARE_geometry_plot.py
+if test -f "./h5m_files/msre.h5m"; then
+  python ./scripts/msre_geometry_plot.py
 else
   python ./scripts/step_to_h5m.py
-  python ./scripts/ARE_geometry_plot.py
+  python ./scripts/msre_geometry_plot.py
 fi
